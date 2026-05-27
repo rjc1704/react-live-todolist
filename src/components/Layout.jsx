c083e36dc52a0a01b5935a090e446ad9e6e4a725
Jecheon Ryu <rjc1704@gmail.com>
2026-05-28 01:39:02 +0900

    수정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
 import styles from "./Layout.module.css";
+import GNB from "./GNB";
 
 export default function Layout() {
   return (
     <div>
       <main className={styles.main}>
+        <GNB />
         <Outlet />
       </main>
     </div>
